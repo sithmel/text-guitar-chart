bcaab72c38bf5ca1c3246ec93d45abe23e7f68d4
Maurizio Lupo <maurizio.lupo@gmail.com>
2025-12-29 22:30:57 +0000

    fix export

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { EditableSVGuitarChord } from "./lib/editableSVGuitar";
-import fingeringToString from "./lib/fingeringToString";
-import stringToFingering from "./lib/stringToFingering";
-import layoutChordStrings from "./lib/layoutChordStrings";
-import splitStringInRectangles from "./lib/splitStringInRectangles";
+import { EditableSVGuitarChord } from "./lib/editableSVGuitar.js";
+import fingeringToString from "./lib/fingeringToString.js";
+import stringToFingering from "./lib/stringToFingering.js";
+import layoutChordStrings from "./lib/layoutChordStrings.js";
+import splitStringInRectangles from "./lib/splitStringInRectangles.js";
 
 export { EditableSVGuitarChord, fingeringToString, stringToFingering, layoutChordStrings, splitStringInRectangles };
