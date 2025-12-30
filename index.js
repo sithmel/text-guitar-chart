8a47c6c9d8bea57498a62545626e55efa71fbe28
Maurizio Lupo <maurizio.lupo@gmail.com>
2025-12-30 21:52:32 +0000

    add dot colors to exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { EditableSVGuitarChord } from "./lib/editableSVGuitar.js";
+import { EditableSVGuitarChord, DOT_COLORS } from "./lib/editableSVGuitar.js";
 import fingeringToString from "./lib/fingeringToString.js";
 import stringToFingering from "./lib/stringToFingering.js";
 import layoutChordStrings from "./lib/layoutChordStrings.js";
 import splitStringInRectangles from "./lib/splitStringInRectangles.js";
 
-export { EditableSVGuitarChord, fingeringToString, stringToFingering, layoutChordStrings, splitStringInRectangles };
+export { EditableSVGuitarChord, fingeringToString, stringToFingering, layoutChordStrings, splitStringInRectangles, DOT_COLORS };
